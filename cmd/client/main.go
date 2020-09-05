b1ef5ff34ad83821bb34f7c4794495ef34b11092
zhouletianvip6 <zhouletian1234@live.com>
2020-09-05 19:28:55 +0800

    add client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,8 +1,26 @@
 package main
 
-import "log"
-
+import (
+	"crypto/tls"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+const(
+	Url = "https://localhost:9090/"
+)
 func main() {
 	log.Println("Start http client...")
-	
+	cli := http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}}
+	resp, err := cli.Get(Url)
+	if err != nil {
+		log.Fatalf("request : %s error:%s",Url,err.Error())
+	}
+	defer resp.Body.Close()
+	io.Copy(os.Stdout,resp.Body)
+
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "log"
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
+const(
+	ServerCrt = "../tls/server.crt"
+	ServerCsr = "../tls/service.csr"
+	ServerKey = "../tls/server.key"
+)
+func Home(w http.ResponseWriter, r *http.Request) {
+	log.Println("method:",r.Method)
+	io.WriteString(w,"Hello world!!!\n")
+
+}
+
+type U struct {
+	 Name string `json:"name"`
+	 Age  int `json:"age"`
+	 Tel  int `json:"tel"`
+}
+func User(w http.ResponseWriter, r *http.Request) {
+	 u := new(U)
+	 u.Name = "LeLe"
+	 u.Age = 20
+	 u.Tel = 15086846070
+	 data, _ := json.Marshal(u)
+     w.Write(data)
+     w.Header().Set("ContentType","application/json")
+     w.WriteHeader(http.StatusOK)
+}
 
 func main() {
 	 log.Println("Start https service...")
+	 http.HandleFunc("/",Home)
+	 http.HandleFunc("/user",User)
+	 if err := http.ListenAndServeTLS(":9090",ServerCrt,ServerKey,nil);err != nil {
+	 	log.Fatal("Start https server error:",err.Error())
+	 }
+
 }
