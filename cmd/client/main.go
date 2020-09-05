0d412c52ee66efda983b5a0f6e7a6a8782bb8182
zhouletianvip6 <zhouletian1234@live.com>
2020-09-06 01:43:01 +0800

    add client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -4,27 +4,75 @@ import (
 	"bytes"
 	"crypto/tls"
 	"encoding/json"
+	"flag"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
-const(
-	Url = "https://localhost:9090/check"
-	)
-var param = []string{"sas","swq","rtet","12","89","77"}
-func main() {
-	log.Println("Start http client...")
+
+const (
+	CheckUrl      = "https://localhost:9090/check"
+	GetAllDataUrl = "https://localhost:9090/data"
+)
+
+var action int
+var data string
+
+func init()  {
+	flag.IntVar(&action,"action",1,"1 get all test data,2 check test data")
+	flag.StringVar(&data,"data","","commit test data and delimit by ,")
+	flag.Parse()
+}
+/**
+ *post test data to server
+ */
+func checkTestData(list []string) {
 	cli := http.Client{
 		Transport: &http.Transport{
 			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 		}}
-	data,_ := json.Marshal(param)
-	resp, err := cli.Post(Url,"application/json;charset=UTF-8", bytes.NewBuffer(data))
+	data, _ := json.Marshal(list)
+	resp, err := cli.Post(CheckUrl, "application/json;charset=UTF-8", bytes.NewBuffer(data))
 	if err != nil {
-		log.Fatalf("code: %d request : %s error:%s",resp.StatusCode,Url,err.Error())
+		log.Fatalf("code: %d request : %s error:%s", resp.StatusCode, CheckUrl, err.Error())
 	}
 	defer resp.Body.Close()
-	io.Copy(os.Stdout,resp.Body)
+	io.Copy(os.Stdout, resp.Body)
+
+}
+
+/**
+ *get all data
+ */
+func getAllData() {
+	cli := http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}}
+	resp, err := cli.Get(GetAllDataUrl)
+	defer resp.Body.Close()
+	if err != nil {
+		log.Fatalf("code: %d request : %s error:%s", resp.StatusCode, CheckUrl, err.Error())
+		return
+	}
+	io.Copy(os.Stdout, resp.Body)
+
+}
+
+func main() {
+	log.Println("action:",action,"data:",data)
+	if action == 1 {
+		getAllData()
+		return
+	}
+
+	list := strings.Split(strings.Trim(data, " "), ",")
+	if len(list) < 1 || data == "" {
+		log.Println("check data at least one")
+		return
+	}
+	checkTestData(list)
 
 }
