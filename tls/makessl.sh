6280c9485f7fa4c2a9093d47548c98bda1b63717
zhouletianvip6 <zhouletian1234@live.com>
2020-09-05 14:36:50 +0800

    add server

--- tls/makessl.sh
+++ tls/makessl.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 openssl genrsa -out server.key 2048
 openssl req -nodes -new -key server.key -subj "/CN=localhost" -out server.crt
-openssl x509 -req -sha256 -days 365 -in server.csr -signkey server.key -out server.crt
+openssl x509 -req -sha256 -days 365 -in server.crt -signkey server.key -out server.csr
