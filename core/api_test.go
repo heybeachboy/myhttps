177c8e92641de035980410e3684b685889fd3deb
zhouletianvip6 <zhouletian1234@live.com>
2020-09-06 12:40:05 +0800

    add benchmark

--- core/api_test.go
+++ core/api_test.go
@@ -26,3 +26,21 @@ func TestCheckData(t *testing.T) {
 	 }
 	 memory.MemStorage.Flush()
 }
+
+func BenchmarkCheckData(b *testing.B) {
+	 in := []string{"wq","wqewqeqeqeqeqeqe","saeqwygu234639449060570sfdsdfsferwrwrwrwrwrwrwwqewqewqweqweqrwerwrwrwrbnvnzxczdsadaewrwretterwqwqwqw",
+	 	"wqweqye211371seweqe34905943570747057375375374573dadwqd457034537sdrksfheuwfe"}
+	 out1 := []bool{false,false,false,false}
+	 out2 := []bool{true,true,true,true}
+	 memory.MemStorage.Flush()
+	 for i:=0; i < b.N; i++ {
+	 	got := CheckData(in)
+	 	if !reflect.DeepEqual(got,out1) && i==0 {
+            b.Errorf("got:%v and expect:%v",got,out1)
+		}
+		 if !reflect.DeepEqual(got,out2) && i>0 {
+            b.Errorf("got:%v and expect:%v",got,out2)
+		 }
+	 }
+	 memory.MemStorage.Flush()
+}
