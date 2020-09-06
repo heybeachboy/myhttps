f3539f8afafb4df6cf831b0e9d58993cd5844eca
zhouletianvip6 <zhouletian1234@live.com>
2020-09-06 12:18:09 +0800

    add unit

--- core/api_test.go
+++ core/api_test.go
@@ -1,1 +1,28 @@
 package core
+
+import (
+	"myhttps/memory"
+	"reflect"
+	"testing"
+)
+
+func TestCheckData(t *testing.T) {
+	 tests:= []struct {
+	 	in []string
+	 	out []bool
+
+	 }{
+	 	 {[]string{"qw","wqwqw","wqw","123"},[]bool{false,false,false,false}},
+		 {[]string{"12","22","33"},[]bool{false,false,false}},
+
+	 }
+	 memory.MemStorage.Flush()
+	 for _,val := range tests {
+	 	got := CheckData(val.in)
+        if !reflect.DeepEqual(got,val.out) {
+        	t.Errorf("got:%v and expect:%v",got,val.out)
+		}
+
+	 }
+	 memory.MemStorage.Flush()
+}
